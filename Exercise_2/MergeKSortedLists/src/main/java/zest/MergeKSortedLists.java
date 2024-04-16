19a425eec72df87fc549f82e8e86df46fbeeb1fe
JoeMei0603 <joel.meier4@uzh.ch>
2024-04-16 18:15:46 +0200

    Solved Task 5 with loads of pain and misery

--- Exercise_2/MergeKSortedLists/src/main/java/zest/MergeKSortedLists.java
+++ Exercise_2/MergeKSortedLists/src/main/java/zest/MergeKSortedLists.java
@@ -1,5 +1,6 @@
 package zest;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 class ListNode {
@@ -14,6 +15,24 @@ public class MergeKSortedLists {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) return null;
 
+        // Check that total amount of nodes is <= 10^4 and values are >=-10^4 and <=10^4
+        int totalNodes = 0;
+        for (ListNode currentNode : lists) {
+            int currentNodeCount = 0;
+            while (currentNode != null) {
+                if (currentNode.val < -10000 || currentNode.val > 10000) {
+                    throw new IllegalArgumentException(
+                            "The node value " + currentNode.val + " is outside the range <-10^5 and >10^5");
+                }
+                currentNodeCount++;
+                currentNode = currentNode.next;
+            }
+            totalNodes += currentNodeCount;
+            if (totalNodes > 10000) {
+                throw new IllegalArgumentException("The total amount of nodes exceeds 10000 entries which is not allowed!");
+            }
+        }
+
         PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, (a, b) -> a.val - b.val);
 
         ListNode dummy = new ListNode(0);
@@ -25,12 +44,27 @@ public class MergeKSortedLists {
             }
         }
 
+        int iterationCount = 0;
+        int currentValue = 0;
         while (!queue.isEmpty()) {
             tail.next = queue.poll();
             tail = tail.next;
 
+            // Ensure that list stays ordered after first iteration during which currentValue is always 0
+            if (iterationCount > 0 && currentValue > tail.val) {
+                throw new RuntimeException("The list is not ordered since a larger value follows a smaller one!");
+            }
+            currentValue = tail.val;
+
             if (tail.next != null) {
                 queue.add(tail.next);
+            }
+
+            if (iterationCount < totalNodes) {
+                iterationCount++;
+            }
+            else {
+                throw new RuntimeException("The amount of iterations is larger than the amount of nodes!");
             }
         }
 
--- Exercise_2/MergeKSortedLists/src/test/java/zest/MergeKSortedListsTest.java
+++ Exercise_2/MergeKSortedLists/src/test/java/zest/MergeKSortedListsTest.java
@@ -1,6 +1,183 @@
 package zest;
 
-class MergeKSortedListsTest {
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
- 
+import java.util.stream.Stream;
+
+class MergeKSortedListsTest {
+    private static MergeKSortedLists classInstance;
+    private ListNode[] lists;
+
+    @BeforeAll
+    static void setupClass() {
+        classInstance = new MergeKSortedLists();
+    }
+
+    @Test
+    void testNullReturn() {
+        ListNode[] emptyInputNode = new ListNode[0];
+        ListNode resultWithNullInput = classInstance.mergeKLists(null);
+        ListNode resultWithEmptyInput = classInstance.mergeKLists(emptyInputNode);
+
+        Assertions.assertNull(resultWithNullInput);
+        Assertions.assertNull(resultWithEmptyInput);
+    }
+
+    @Test
+    void testZeroReturn() {
+        lists = new ListNode[]{
+                new ListNode(),
+                null,
+                new ListNode(1, new ListNode(2))
+        };
+
+        ListNode result = classInstance.mergeKLists(lists);
+
+        Assertions.assertEquals(0, result.val);
+        Assertions.assertNotNull(result.next);
+    }
+
+    @Test
+    void testValidInput() {
+        lists = new ListNode[]{
+                new ListNode(-10000),
+                new ListNode(0, new ListNode(0)),
+                new ListNode(10000)
+        };
+        ListNode result = classInstance.mergeKLists(lists);
+
+        Assertions.assertEquals(-10000, result.val);
+        Assertions.assertEquals(0, result.next.val);
+        Assertions.assertEquals(0, result.next.next.val);
+        Assertions.assertEquals(10000, result.next.next.next.val);
+    }
+
+    @Test
+    void testTooSmallNodeValue() {
+        lists = new ListNode[]{
+                new ListNode(-10001, new ListNode(-5000)),
+                new ListNode(5000, new ListNode(9999))
+        };
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.mergeKLists(lists));
+    }
+
+    @Test
+    void testTooLargeNodeValue() {
+        lists = new ListNode[]{
+                new ListNode(-9999, new ListNode(-5000)),
+                new ListNode(5000, new ListNode(10001))
+        };
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.mergeKLists(lists));
+    }
+
+    @Test
+    void testTooLargeNodeInputAmount() {
+        lists = new ListNode[1];
+
+        ListNode currentNode = null;
+        for (int i = 0; i < 10001; i++) {
+            ListNode nextNode = new ListNode(i);
+            if (currentNode == null) {
+                currentNode = nextNode;
+                lists[0] = currentNode;
+            } else {
+                currentNode.next = nextNode;
+                currentNode = nextNode;
+            }
+        }
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.mergeKLists(lists));
+    }
+
+    @Test
+    void testValidLargeNodeInputAmount() {
+        lists = new ListNode[1];
+
+        ListNode currentNode = null;
+        for (int i = 0; i < 10000; i++) {
+            ListNode nextNode = new ListNode(i);
+            if (currentNode == null) {
+                currentNode = nextNode;
+                lists[0] = currentNode;
+            } else {
+                currentNode.next = nextNode;
+                currentNode = nextNode;
+            }
+        }
+        ListNode result = classInstance.mergeKLists(lists);
+        int resultSize = getSize(result);
+
+        Assertions.assertEquals(10000, resultSize);
+    }
+
+    private int getSize(ListNode resultNode) {
+        int count = 0;
+        ListNode currentNode = resultNode;
+        while (currentNode != null){
+            count++;
+            currentNode = currentNode.next;
+        }
+        return count;
+    }
+
+    @Property
+    void testValidProperties(@ForAll("validLists") ListNode[] validLists) {
+        MergeKSortedLists propertyClassInstance = new MergeKSortedLists();
+        ListNode result = propertyClassInstance.mergeKLists(validLists);
+
+        Assertions.assertTrue(result.val <= result.next.val);
+        Assertions.assertTrue(result.next.val <= result.next.next.val);
+    }
+
+    @Property
+    void testInValidProperties(@ForAll("invalidLists") ListNode[] invalidLists) {
+        MergeKSortedLists propertyClassInstance = new MergeKSortedLists();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> propertyClassInstance.mergeKLists(invalidLists));
+    }
+
+    @Property
+    void testDifferentValidListLengths(@ForAll @IntRange(min = 0, max = 10000) int n) {
+        MergeKSortedLists propertyClassInstance = new MergeKSortedLists();
+        ListNode[] lists = generateRandomListNodes(n);
+        ListNode result = propertyClassInstance.mergeKLists(lists);
+
+        Assertions.assertEquals(n, getSize(result));
+    }
+
+    @Provide
+    Arbitrary<ListNode[]> validLists() {
+        Arbitrary<Integer> nodeValue1 = Arbitraries.integers().between(-10000, 10000);
+        Arbitrary<Integer> nodeValue2 = Arbitraries.integers().between(-10000, 10000);
+        Arbitrary<Integer> nodeValue3 = Arbitraries.integers().between(-10000, 10000);
+
+        return Combinators.combine(nodeValue1, nodeValue2, nodeValue3)
+                .as((node1, node2, node3) -> new ListNode[]
+                        {new ListNode(node1),
+                         new ListNode(node2),
+                         new ListNode(node3)});
+    }
+
+    @Provide
+    Arbitrary<ListNode[]> invalidLists() {
+        Arbitrary<Integer> nodeValue1 = Arbitraries.integers().between(-10000, 10000);
+        Arbitrary<Integer> tooSmallValue = Arbitraries.integers().lessOrEqual(-10001);
+        Arbitrary<Integer> tooLargeValue = Arbitraries.integers().greaterOrEqual(10001);
+
+        return Combinators.combine(nodeValue1, Arbitraries.oneOf(tooSmallValue, tooLargeValue))
+                .as((node1, node2) -> new ListNode[]
+                        {new ListNode(node1, new ListNode(node2))});
+    }
+
+    private ListNode[] generateRandomListNodes(int size) {
+        return Stream.generate(() -> new ListNode(1))
+                .limit(size)
+                .toArray(ListNode[]::new);
+    }
 }
