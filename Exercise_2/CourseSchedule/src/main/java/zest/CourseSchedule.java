3cb310a54b0e6ced0dcfc039c8be1944cf6f1903
jemaie <jerome.maier@uzh.ch>
2024-04-17 11:41:08 +0200

    Task 4 adjustments

--- Exercise_2/CourseSchedule/src/main/java/zest/CourseSchedule.java
+++ Exercise_2/CourseSchedule/src/main/java/zest/CourseSchedule.java
@@ -5,22 +5,15 @@ import java.util.List;
 
 public class CourseSchedule {
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (numCourses < 0) {
-            throw new IllegalArgumentException("Cannot visit a negative amount of courses!");
-        }
+        if (numCourses < 0) throw new IllegalArgumentException("Cannot visit a negative amount of courses!");
         for (int[] prerequisite : prerequisites) {
-            if (prerequisite.length == 0) {
-                throw new IllegalArgumentException("Cannot provide empty arrays!");
-            }
-            if (prerequisite[0] < 0 || prerequisite[0] >= numCourses) {
+            if (prerequisite.length == 0) throw new IllegalArgumentException("Cannot provide empty arrays!");
+            if (prerequisite[0] < 0 || prerequisite[0] >= numCourses)
                 throw new IllegalArgumentException("Course " + prerequisite[0] + " does not exist!");
-            }
-            if (prerequisite[1] < 0 || prerequisite[1] >= numCourses) {
+            if (prerequisite[1] < 0 || prerequisite[1] >= numCourses)
                 throw new IllegalArgumentException("Course " + prerequisite[1] + " does not exist!");
-            }
-            if (prerequisite[0] == prerequisite[1]) {
+            if (prerequisite[0] == prerequisite[1])
                 throw new IllegalArgumentException("Courses cannot be dependent on themselves!");
-            }
         }
 
 
@@ -29,18 +22,15 @@ public class CourseSchedule {
         for (int i = 0; i < numCourses; i++) {
             graph.add(new ArrayList<>());
         }
-        if (graph.size() != numCourses) {
-            throw new RuntimeException("Length of graph list is wrong!");
-        }
+        if (graph.size() != numCourses) throw new RuntimeException("Length of graph list is wrong!");
 
         for (int[] prerequisite : prerequisites) {
             graph.get(prerequisite[1]).add(prerequisite[0]);
 
             List<Integer> prerequisiteList = graph.get(prerequisite[1]);
             if (prerequisiteList.get(prerequisiteList.size() - 1) < 0 ||
-                    prerequisiteList.get(prerequisiteList.size() - 1) >= numCourses) {
+                    prerequisiteList.get(prerequisiteList.size() - 1) >= numCourses)
                 throw new RuntimeException("A list with an invalid course number was appended to the graph!");
-            }
         }
 
         boolean[] visited = new boolean[numCourses];
@@ -51,12 +41,8 @@ public class CourseSchedule {
                 return false; // Cycle detected
             }
 
-            if (!visited[i]) {
-                throw new RuntimeException("A visited node is set as not visited!");
-            }
-            if (onPath[i]) {
-                throw new RuntimeException("A node is still set as onPath after being visited!");
-            }
+            if (!visited[i]) throw new RuntimeException("A visited node is set as not visited!");
+            if (onPath[i]) throw new RuntimeException("A node is still set as onPath after being visited!");
         }
         return true; // No cycle detected
     }
@@ -73,12 +59,8 @@ public class CourseSchedule {
                 return true;
             }
 
-            if (!visited[current]) {
-                throw new RuntimeException("A node currently being visited is set as not visited!");
-            }
-            if (!onPath[current]) {
-                throw new RuntimeException("A node is not set as onPath while being visited!");
-            }
+            if (!visited[current]) throw new RuntimeException("A node currently being visited is set as not visited!");
+            if (!onPath[current]) throw new RuntimeException("A node is not set as onPath while being visited!");
         }
 
         onPath[current] = false; // Backtrack
