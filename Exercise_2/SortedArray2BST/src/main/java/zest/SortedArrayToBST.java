35acfe8ebbf72624796a27f18cd483f0db314ed4
JoeMei0603 <joel.meier4@uzh.ch>
2024-04-20 15:39:41 +0200

    Solved Task 6 and refactored something in Task 5.

--- Exercise_2/SortedArray2BST/src/main/java/zest/SortedArrayToBST.java
+++ Exercise_2/SortedArray2BST/src/main/java/zest/SortedArrayToBST.java
@@ -1,8 +1,5 @@
 package zest;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 class TreeNode {
     int val;
@@ -14,7 +11,20 @@ class TreeNode {
 public class SortedArrayToBST {
 
     public zest.TreeNode sortedArrayToBST(int[] nums) {
-        return constructBSTRecursive(nums, 0, nums.length - 1);
+        // Check pre-conditions
+        checkIfArrayNull(nums);
+        if (nums.length == 0) {
+            return null;
+        }
+        checkIfArrayTooLong(nums);
+        checkIfArrayIsSortedAndUnique(nums);
+
+        TreeNode resultingBSTTree = constructBSTRecursive(nums, 0, nums.length - 1);
+
+        // Check post-conditions
+        compareInputAndResultSize(nums, resultingBSTTree);
+
+        return resultingBSTTree;
     }
 
     private zest.TreeNode constructBSTRecursive(int[] nums, int left, int right) {
@@ -26,6 +36,16 @@ public class SortedArrayToBST {
         zest.TreeNode node = new zest.TreeNode(nums[mid]);
         node.left = constructBSTRecursive(nums, left, mid - 1);
         node.right = constructBSTRecursive(nums, mid + 1, right);
+
+        // Check invariants before returning node
+        checkLeftAndRightNode(node);
+        int rightDepth = getNodeDepth(node.right);
+        int leftDepth= getNodeDepth(node.left);
+        int heightDiff = rightDepth - leftDepth;
+        if (Math.abs(heightDiff) > 1) {
+            throw new IllegalArgumentException("Depth difference between left and right node is larger than 1!");
+        }
+
         return node;
     }
 
@@ -51,5 +71,60 @@ public class SortedArrayToBST {
         }
 
         return result;
+    }
+
+    private void checkIfArrayNull(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("The input array nums may not be null!");
+        }
+    }
+
+    private void checkIfArrayTooLong(int[] nums) {
+        if (nums.length > 10000) {
+            throw new IllegalArgumentException("The input array nums has more than 10^4 entries!");
+        }
+    }
+
+    private void checkIfArrayIsSortedAndUnique(int[] nums) {
+        Set<Integer> uniqueInputSet = new HashSet<>();
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i+1]) {
+                throw new IllegalArgumentException("The input array nums must be in ascending order!");
+            }
+            if (!uniqueInputSet.add(nums[i])) {
+                throw new IllegalArgumentException("The input array nums contain unique integers!");
+            }
+        }
+
+        // Check last element for uniqueness as well
+        if (!uniqueInputSet.add(nums[nums.length - 1])) {
+            throw new IllegalArgumentException("The input array nums contains unique integers!");
+        }
+    }
+
+    private void compareInputAndResultSize(int[] nums, TreeNode resultBSTTree) {
+        int treeNodeLength = levelOrderTraversal(resultBSTTree).size();
+        if (nums.length != treeNodeLength) {
+            throw new IllegalArgumentException("The input array and resulting tree do not have the same amount of integers!");
+        }
+    }
+
+    private void checkLeftAndRightNode(TreeNode nodeToCheck) {
+        if (nodeToCheck.left != null && nodeToCheck.left.val >= nodeToCheck.val) {
+            throw new IllegalArgumentException("The left-child node is larger than its parent-node.");
+        }
+
+        if (nodeToCheck.right != null && nodeToCheck.right.val <= nodeToCheck.val) {
+            throw new IllegalArgumentException("The left-child node is larger than its parent-node.");
+        }
+    }
+
+    private int getNodeDepth(TreeNode root)
+    {
+        if(root!=null)
+            return 1+ Math.max(getNodeDepth(root.left), getNodeDepth(root.right));
+        else
+            return 0;
     }
 }
--- Exercise_2/SortedArray2BST/src/test/java/zest/SortedArrayToBSTTest.java
+++ Exercise_2/SortedArray2BST/src/test/java/zest/SortedArrayToBSTTest.java
@@ -1,6 +1,149 @@
 package zest;
 
+import net.jqwik.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class SortedArrayToBSTTest {
 
- 
+    private static SortedArrayToBST classInstance;
+
+    @BeforeAll
+    static void setupClass() {
+        classInstance = new SortedArrayToBST();
+    }
+
+    @Test
+    void testEmptyArray() {
+        int[] nums = {};
+        TreeNode treeResult = classInstance.sortedArrayToBST(nums);
+        List<Integer> listResult = classInstance.levelOrderTraversal(treeResult);
+
+        Assertions.assertNull(treeResult);
+        Assertions.assertEquals(listResult.size(), 0);
+    }
+
+    @Test
+    void testArrayOne() {
+        int[] nums = {1};
+        TreeNode treeResult = classInstance.sortedArrayToBST(nums);
+        List<Integer> listResult = classInstance.levelOrderTraversal(treeResult);
+
+        Assertions.assertEquals(listResult.size(), 1);
+        Assertions.assertEquals(listResult.get(0), 1);
+    }
+
+    @Test
+    void testSimpleArray() {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        TreeNode treeResult = classInstance.sortedArrayToBST(nums);
+        List<Integer> listResult = classInstance.levelOrderTraversal(treeResult);
+
+        int root = listResult.get(0);
+
+        Assertions.assertEquals(root, 4);
+    }
+
+    @Test
+    void testArrayNull() {
+        int[] nums = null;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.sortedArrayToBST(nums));
+    }
+
+    @Test
+    void testArrayTooLong() {
+        int[] nums = createTooLargeArray();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.sortedArrayToBST(nums));
+    }
+
+    @Test
+    void testArrayLengthBoundary() {
+        int[] nums = createLargeArray();
+        TreeNode treeResult = classInstance.sortedArrayToBST(nums);
+        List<Integer> listResult = classInstance.levelOrderTraversal(treeResult);
+
+        Assertions.assertEquals(listResult.size(), 10000);
+    }
+
+    @Test
+    void testNonSortedArray() {
+        int[] nums = {1, 2, 3, 4, 5, 7, 6};
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.sortedArrayToBST(nums));
+    }
+
+    @Test
+    void testNonUniqueArray() {
+        int[] nums = {1, 2, 3, 3, 4, 5};
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.sortedArrayToBST(nums));
+    }
+
+    @Test
+    void testNonUniqueArrayAtEnd() {
+        int[] nums = {1, 2, 3, 4, 5, 6, 6};
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.sortedArrayToBST(nums));
+    }
+
+    private static int[] createTooLargeArray() {
+        int[] nums = new int[10001];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i;
+        }
+        return nums;
+    }
+
+    private static int[] createLargeArray() {
+        int[] nums = new int[10000];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i;
+        }
+        return nums;
+    }
+
+    @Property
+    void testRandomValidIntegers(@ForAll("createListOfValidIntegers") int[] nums) {
+        SortedArrayToBST classInstance = new SortedArrayToBST();
+        TreeNode treeResult = classInstance.sortedArrayToBST(nums);
+        int numsLength = nums.length;
+        List<Integer> listResult = classInstance.levelOrderTraversal(treeResult);
+
+        Assertions.assertEquals(numsLength, listResult.size());
+    }
+
+    @Property(tries = 10)
+    void testInvalidArrayLengths(@ForAll("createInvalidArrayLength") int[] nums) {
+        SortedArrayToBST classInstance = new SortedArrayToBST();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> classInstance.sortedArrayToBST(nums));
+    }
+
+    @Provide
+    Arbitrary<int[]> createListOfValidIntegers() {
+        return Arbitraries.integers().array(int[].class).ofMinSize(0).ofMaxSize(10000)
+                .map(integers -> {
+                    Set<Integer> uniqueSet = new HashSet<>();
+                    for (int i: integers){
+                        uniqueSet.add(i);
+                    }
+                    int[] uniqueArray = uniqueSet.stream().mapToInt(Integer::intValue).toArray();
+                    Arrays.sort(uniqueArray);
+                    return uniqueArray;
+                });
+    }
+
+    @Provide
+    Arbitrary<int[]> createInvalidArrayLength(){
+        return Arbitraries.integers().array(int[].class).ofMinSize(10001)
+                .map(integers -> integers);
+    }
 }
