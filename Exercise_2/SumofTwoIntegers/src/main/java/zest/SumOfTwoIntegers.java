1863646125b54ae283baca7e367db6fbe4755e63
FaizanNehal <faizannehal99@gmail.com>
2024-04-22 14:56:23 +0200

    Exercise 7 Tests & Solution updated

--- Exercise_2/SumofTwoIntegers/src/main/java/zest/SumOfTwoIntegers.java
+++ Exercise_2/SumofTwoIntegers/src/main/java/zest/SumOfTwoIntegers.java
@@ -1,7 +1,7 @@
 package zest;
 
 public class SumOfTwoIntegers {
-    public int getSum(int a, int b) {
+    public long getSum(long a, long b) {
 
         // Pre-condition
         if (a > Integer.MAX_VALUE || a < Integer.MIN_VALUE ) {
@@ -10,11 +10,12 @@ public class SumOfTwoIntegers {
         if (b > Integer.MAX_VALUE || b < Integer.MIN_VALUE ) {
             throw new IllegalArgumentException("Input `b` is out of range");
         }
-        int a_initial=a;
-        int b_initial=b;
+        long a_initial=a;
+        long b_initial=b;
+
 
         while (b != 0) {
-            int carry = (a & b) << 1;  // Carry now contains common set bits of a and b
+            long carry = (a & b) << 1;  // Carry now contains common set bits of a and b
             a = a ^ b;  // Sum of bits of a and b where at least one of the bits is not set
             b = carry;  // Carry is shifted by one so that adding it to a gives the required sum
         }
--- Exercise_2/SumofTwoIntegers/src/test/java/zest/SumOfTwoIntegersTest.java
+++ Exercise_2/SumofTwoIntegers/src/test/java/zest/SumOfTwoIntegersTest.java
@@ -1,8 +1,8 @@
 package zest;
 
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
+import net.jqwik.api.*;
 import net.jqwik.api.constraints.IntRange;
+import net.jqwik.api.constraints.LongRange;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SumOfTwoIntegersTest {
+    // 2147483647  -2,147,483,648
     @Test
     void testSumofTwoPostiveIntegers() {
         SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
-        int result = testSumClass.getSum(2,3);
+        long result = testSumClass.getSum(2,3);
 
         Assertions.assertEquals(5, result);
     }
@@ -21,7 +22,7 @@ class SumOfTwoIntegersTest {
     @Test
     void testSumofTwoNegativeIntegers() {
         SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
-        int result = testSumClass.getSum(-2,-5);
+        long result = testSumClass.getSum(-2,-5);
 
         Assertions.assertEquals(-7, result);
     }
@@ -29,20 +30,83 @@ class SumOfTwoIntegersTest {
     @Test
     void testSumofPositiveAndZero() {
         SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
-        int result = testSumClass.getSum(2,0);
+        long result = testSumClass.getSum(2,0);
 
         Assertions.assertEquals(2, result);
     }
 
+    @Test
+    void testSumofTwoIntegersResultGreaterThanMaxINT() {
+        SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
+        long result = testSumClass.getSum(2147483647,3);
+
+        Assertions.assertEquals(2147483650L, result);
+    }
+
+    @Test
+    void testSumofTwoIntegersPassingAGreaterThanMaxINT() {
+        SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
+
+        assertThrows(IllegalArgumentException.class, () -> testSumClass.getSum(2147483648L,6));
+
+    }
+
+    @Test
+    void testSumofTwoIntegersPassingBGreaterThanMaxINT() {
+        SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
+
+        assertThrows(IllegalArgumentException.class, () -> testSumClass.getSum(1,2147483648L));
+
+    }
+
 
     @Property
-    void testSumofTwoIntegerProperty(@ForAll @IntRange(max = Integer.MAX_VALUE, min = Integer.MIN_VALUE) int a,@ForAll @IntRange(max = Integer.MAX_VALUE, min = Integer.MIN_VALUE) int b) {
+    void testSumofTwoIntegerProperty(@ForAll @LongRange(max = Integer.MAX_VALUE, min = Integer.MIN_VALUE) long a, @ForAll @LongRange(max = Integer.MAX_VALUE, min = Integer.MIN_VALUE) long b) {
 
         SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
-        int result = testSumClass.getSum(a,b);
+        long result = testSumClass.getSum(a,b);
 
         assertEquals(a+b, result);
     }
+
+    @Property
+    void testSumofTwoInvalidIntegerProperty(@ForAll("invalidArguments") long a, @ForAll("invalidArguments") long b) {
+
+        SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
+
+        assertThrows(IllegalArgumentException.class, () -> testSumClass.getSum(a,b));
+
+    }
+
+    @Property
+    void testSumWithInvalidAProperty(@ForAll("invalidArguments") long a, @ForAll @LongRange(max = Integer.MAX_VALUE, min = Integer.MIN_VALUE) long b) {
+
+        SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
+
+        assertThrows(IllegalArgumentException.class, () -> testSumClass.getSum(a,b));
+
+    }
+
+
+    @Property
+    void testSumWithInvalidBProperty(@ForAll @LongRange(max = Integer.MAX_VALUE, min = Integer.MIN_VALUE) long a, @ForAll("invalidArguments") long b) {
+
+        SumOfTwoIntegers testSumClass = new SumOfTwoIntegers();
+
+        assertThrows(IllegalArgumentException.class, () -> testSumClass.getSum(a,b));
+
+    }
+
+
+    @Provide
+    private Arbitrary<Long> invalidArguments() {
+        return Arbitraries.oneOf(
+                Arbitraries.longs().lessOrEqual(-2147483649L),
+                Arbitraries.longs().greaterOrEqual(2147483648L)
+        );
+    }
+
+
 
 
 
